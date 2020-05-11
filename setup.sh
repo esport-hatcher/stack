5a7341ab0cd019768e0e68f34a7d127d0eb5770f
Arthur <arthur.gamblin@epitech.eu>
2020-05-11 01:03:21 -0400

    refacto(deployment): connected CI script to GC

--- setup.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/sh
-minikube delete
-minikube start --cpus 4 --memory 8192
-kubectl create secret generic eh-secrets --from-env-file=./secrets.txt
-kubectl apply -f ./k8s/persistent
-skaffold dev
