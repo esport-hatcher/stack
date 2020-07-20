87e9e682d6fa3f62a19dd37e06e262fa794c3661
Yukeien <yukeiensan@gmail.com>
2020-07-20 18:18:30 -0400

    Local CI dockerized

---
+++ run.sh
@@ -0,0 +1,6 @@
+#! /bin/bash
+
+minikube start --cpus 4 --memory 8192
+kubectl create secret generic eh-secrets --from-env-file=./secrets.txt
+kubectl apply -f k8s/persistent
+skaffold dev
---
+++ testing.sh
@@ -0,0 +1,9 @@
+#! /bin/bash
+docker network create travis-tests
+docker build -t agamblin/eh-server-test -f ./server/Dockerfile.dev ./server
+docker create --name mysqltest -e MYSQL_ROOT_PASSWORD=root --network=travis-tests mysql/mysql-server:5.7
+docker cp my.cnf mysqltest:/etc/
+docker start mysqltest
+
+docker create --name servertest -e AWS_ACCESS_KEY=$S3_AWS_ACCESS_KEY -e AWS_SECRET_KEY=$S3_AWS_SECRET_KEY -e AWS_BUCKET_NAME=$S3_AWS_BUCKET_NAME -e SQL_HOST=mysqltest -e SQL_PORT=3306 -e SQL_USER=root -e SQL_PASSWORD=root -e SQL_DB=eh_testing -e NODE_ENV=CI -e JWT_SECRET=somejwtrandom --network=travis-tests agamblin/eh-server-test yarn launch-tests --coverage
+docker start servertest
